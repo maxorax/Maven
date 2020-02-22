cbfb2c75ae3d3976cc2a897a9afa81c25a655c22
maxorax <orlov3020max@gmail.com>
2020-02-22 22:44:02 +0400

    done

---
+++ src/test/java/ru/orlov/maxim/AppTest.java
@@ -0,0 +1,23 @@
+package ru.orlov.maxim;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    private void assertTrue(boolean b) {
+    }
+}
